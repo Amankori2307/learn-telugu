f108f25d1844f19c5f523850439477be7d4913b2
Aman Kori <aman@napqueens.com>
2024-08-28 01:14:10 +0530

    Created basic UI

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,22 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="learn-telugu" />
+    <div className={styles.mainWrapper}>
+      <div className={styles.containerWrapper}>
+        <div className={styles.container}>
+          <p className={styles.wordWrapper}><b><span className={styles.word}>Word</span><span className={styles.pronunciation}><i>(Pronunciation)</i></span></b></p>
+
+          <ul className={styles.optionList}>
+            <li className={styles.option}>Option 1</li>
+            <li className={styles.option}>Option 2</li>
+            <li className={styles.option}>Option 3</li>
+            <li className={styles.option}>Option 4</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
